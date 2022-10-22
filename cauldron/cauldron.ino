41b0005c753146ab03fbf9e6f3319f5af2f5b757
Aaron Lau <aaron.t.y.lau@gmail.com>
2022-10-21 22:28:13 -0600

    Set up pointers to skip null pixels

--- cauldron/cauldron.ino
+++ cauldron/cauldron.ino
@@ -8,21 +8,41 @@
   https://www.holidaycoro.com/kb_results.asp?ID=156
   There are 5 on strand A and 4 on strand B
 */
-#define ASTRAND_START_LED 5
-#define ASTRAND_END_LED 255
-#define BSTRAND_START_LED 4
-#define BSTRAND_END_LED 254
-
+#define ASTRAND_START_LED 4
+#define ASTRAND_END_LED 254
+#define BSTRAND_START_LED 3
+#define BSTRAND_END_LED 253
+#define ASTRAND_TOTAL_LEN ASTRAND_END_LED + 1
+#define BSTRAND_TOTAL_LEN BSTRAND_END_LED + 1
+#define ASTRAND_SPIRAL_LENGTH ASTRAND_END_LED-ASTRAND_START_LED
+#define BSTRAND_SPIRAL_LENGTH BSTRAND_END_LED-BSTRAND_START_LED
 #define SPIRAL_LENGTH 250
 
-CRGB astrand[ASTRAND_END_LED - ASTRAND_START_LED];
-CRGB bstrand[BSTRAND_END_LED - BSTRAND_START_LED];
+CRGB a_physical_strand[ASTRAND_END_LED + 1];
+CRGB b_physical_strand[BSTRAND_END_LED + 1];
+
+CRGB* astrand[ASTRAND_SPIRAL_LENGTH];
+CRGB* bstrand[BSTRAND_SPIRAL_LENGTH];
 
 // put your setup code here, to run once:
 void setup() {
-  // TODO(eric): need to skip over the null pixels
-  FastLED.addLeds<WS2811, ASTRAND_DATA_PIN>(astrand, SPIRAL_LENGTH);
-  FastLED.addLeds<WS2811, BSTRAND_DATA_PIN>(bstrand, SPIRAL_LENGTH);
+  FastLED.addLeds<WS2811, ASTRAND_DATA_PIN>(a_physical_strand, ASTRAND_TOTAL_LEN + 1);
+  FastLED.addLeds<WS2811, BSTRAND_DATA_PIN>(b_physical_strand, BSTRAND_TOTAL_LEN + 1);
+
+  populate_astrand();
+  populate_bstrand();
+}
+
+void populate_astrand() {
+  for(int i = ASTRAND_SPIRAL_LENGTH; i > 0; i--) {
+    astrand[ASTRAND_SPIRAL_LENGTH-i] = &a_physical_strand[i + ASTRAND_START_LED];
+  }
+}
+
+void populate_bstrand() {
+  for(int i = BSTRAND_SPIRAL_LENGTH; i > 0; i--) {
+    bstrand[BSTRAND_SPIRAL_LENGTH-i] = &b_physical_strand[i + BSTRAND_START_LED];
+  }
 }
 
 // put your main code here, to run repeatedly:
@@ -31,9 +51,9 @@ void loop() {
 }
 
 void cycle(CRGB color, int delayMS) {
-  for (int i = SPIRAL_LENGTH; i > 0; i--) {
-    astrand[i] = color;
-    bstrand[i] = color;
+  for (int i = 0; i < ASTRAND_SPIRAL_LENGTH; i++) {
+    a_physical_strand[i] = color;
+    b_physical_strand[i] = color;
     FastLED.show();
     delay(delayMS);
   }
